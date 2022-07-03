98ab03020d6da529d7a33340c18d4b24386032e8
James Hall <jdhall@live.co.uk>
2022-07-03 14:33:27 +0800

    created the chat channel

--- config/importmap.rb
+++ config/importmap.rb
@@ -10,3 +10,5 @@ pin "@rails/actioncable", to: "https://ga.jspm.io/npm:@rails/actioncable@6.0.5/a
 pin "cable_ready", to: "https://ga.jspm.io/npm:cable_ready@5.0.0-pre8/javascript/index.js"
 pin "morphdom", to: "https://ga.jspm.io/npm:morphdom@2.6.1/dist/morphdom.js"
 pin "stimulus", to: "https://ga.jspm.io/npm:stimulus@3.0.1/dist/stimulus.js"
+pin "@rails/actioncable", to: "actioncable.esm.js"
+pin_all_from "app/javascript/channels", under: "channels"
